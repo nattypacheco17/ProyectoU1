85a0b31def3ccc8b0f00eec86d62503e160dc9ec
NATALY MARISOL PACHECO RAMIREZ <168054927+nattypacheco17@users.noreply.github.com>
2024-12-12 18:58:51 -0500

    carga de pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { GameCodeComponent } from './game-code/game-code.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { NgModule } from '@angular/core';
+import { PrincipalComponent } from './principal/principal.component';
 
 export const routes: Routes = [
+  { path: '', component: PrincipalComponent},
     { path: 'game-code', component: GameCodeComponent},
     { path: 'componente2', component: Componente2Component},
 ];
