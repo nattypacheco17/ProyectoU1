77cc60fc47b8ac781e52ba0fad6275875605bdc6
SteveenStopper <84851263+SteveenStopper@users.noreply.github.com>
2024-12-12 11:35:22 -0500

    Game Code Componet
    UI to join via game code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { GameCodeComponent } from './game-code/game-code.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'game-code', component: GameCodeComponent},
+    { path: 'componente2', component: Componente2Component},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
