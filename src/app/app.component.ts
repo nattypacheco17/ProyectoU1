b524c335503bafa83f940e50e815d1b371d06ec9
SteveenStopper <84851263+SteveenStopper@users.noreply.github.com>
2024-12-12 11:35:22 -0500

    Componet Game Code
    UI to join via game code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Componente2Component } from './componente2/componente2.component';
+import { GameCodeComponent } from './game-code/game-code.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente2Component],
+  imports: [RouterOutlet, Componente2Component, GameCodeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
