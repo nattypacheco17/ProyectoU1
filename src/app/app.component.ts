9f1847ce9e3c20b5982bf6ee3b871542c424393d
NICOLAS LARA <nalara2@espe.edu.ec>
2024-12-12 18:42:50 -0500

    Componente5 modificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Componente2Component } from './componente2/componente2.component';
 import { GameCodeComponent } from './game-code/game-code.component';
 import { TablaComponent } from './tabla/tabla.component';
+import { Componente5Component } from './componente5/componente5.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente2Component, GameCodeComponent, TablaComponent],
+  imports: [RouterOutlet, Componente2Component, GameCodeComponent, Componente5Component, TablaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
