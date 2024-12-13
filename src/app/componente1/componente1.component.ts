8d8b9c50a9dc1543d714c0514525ca717181307b
NATALY MARISOL PACHECO RAMIREZ <168054927+nattypacheco17@users.noreply.github.com>
2024-12-13 02:06:33 -0500

    animaciones

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -6,25 +6,35 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-componente1',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule,RouterLink],
   templateUrl: './componente1.component.html',
-  styleUrls: ['./componente1.component.css']
-
+  styleUrls: ['./componente1.component.css'],
 })
 export class Componente1Component {
   nombre: string = '';
-  numJugadores: number = 2; // Por defecto a 2 jugadores
+  numJugadores: number = 2;
+  jugadores = [
+    { value: 2, label: '2 jugadores' },
+    { value: 3, label: '3 jugadores' },
+    { value: 4, label: '4 jugadores' },
+    { value: 5, label: '5 jugadores' },
+    { value: 6, label: '6 jugadores' },
+  ];
 
-  @Output() juegoCreado = new EventEmitter<{ nombre: string, numJugadores: number }>();
+  @Output() juegoCreado = new EventEmitter<{ nombre: string; numJugadores: number }>();
 
   crearJuego() {
+    if (this.nombre.trim() === '') {
+      alert('Por favor, ingresa tu nombre.');
+      return;
+    }
     this.juegoCreado.emit({
       nombre: this.nombre,
-      numJugadores: this.numJugadores
+      numJugadores: this.numJugadores,
     });
   }
 
   unirseJuego() {
-    console.log("Funcionalidad para unirse a un juego");
+    console.log('Funcionalidad para unirse a un juego');
   }
 }
