45fe7490805ccb4c90d91a387ebb56957da89cd2
SteveenStopper <84851263+SteveenStopper@users.noreply.github.com>
2024-12-12 22:20:57 -0500

    Rute Component

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
