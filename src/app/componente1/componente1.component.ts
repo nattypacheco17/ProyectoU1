b0ceda3aaba3222d40615ea3e09167357a3aca56
NATALY MARISOL PACHECO RAMIREZ <168054927+nattypacheco17@users.noreply.github.com>
2024-12-12 22:33:43 -0500

    componente 1 codificacion

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-componente1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './componente1.component.html',
-  styleUrl: './componente1.component.css'
+  styleUrls: ['./componente1.component.css']
+
 })
 export class Componente1Component {
+  nombre: string = '';
+  numJugadores: number = 2; // Por defecto a 2 jugadores
 
+  @Output() juegoCreado = new EventEmitter<{ nombre: string, numJugadores: number }>();
+
+  crearJuego() {
+    this.juegoCreado.emit({
+      nombre: this.nombre,
+      numJugadores: this.numJugadores
+    });
+  }
+
+  unirseJuego() {
+    console.log("Funcionalidad para unirse a un juego");
+  }
 }
