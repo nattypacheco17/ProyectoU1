aae83684032e5d06e3bb5e2c6c50ceabf739ce51
SteveenStopper <84851263+SteveenStopper@users.noreply.github.com>
2025-01-14 12:58:34 -0500

    Game-code component update

--- src/app/game-code/game-code.component.ts
+++ src/app/game-code/game-code.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -5,16 +6,17 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-game-code',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './game-code.component.html',
   styleUrls: ['./game-code.component.scss'],
 })
 export class GameCodeComponent {
-showInstructions() {
-throw new Error('Metodo no impelmentado.');
-}
   gameCode: string = '';
   characterCount: number = 0;
+
+  showInstructions() {
+    throw new Error('Metodo no impelmentado.');
+  }
 
   updateCharacterCount(): void {
     this.characterCount = this.gameCode.length;
@@ -24,5 +26,9 @@ throw new Error('Metodo no impelmentado.');
     if (this.gameCode.length === 6) {
       console.log('Buscando juego con código:', this.gameCode);
     }
+  }
+
+  isValidCode(): boolean {
+    return this.gameCode.length === 6;
   }
 }
