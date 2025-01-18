3fd077146d3c2fc48d96879c4175bd1516cfdced
NATALY MARISOL PACHECO RAMIREZ <168054927+nattypacheco17@users.noreply.github.com>
2025-01-18 16:10:51 -0500

    creacion componente salaa espera

---
+++ src/app/sala-espera/sala-espera.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sala-espera',
+  standalone: true,
+  imports: [],
+  templateUrl: './sala-espera.component.html',
+  styleUrl: './sala-espera.component.css'
+})
+export class SalaEsperaComponent {
+
+}
