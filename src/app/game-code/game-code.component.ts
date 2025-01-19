5f9e6c6bd8481d4c47190b47ac9cce7a34a1a2f4
NATALY MARISOL PACHECO RAMIREZ <168054927+nattypacheco17@users.noreply.github.com>
2025-01-18 20:29:42 -0500

    responsive componente 1

--- src/app/game-code/game-code.component.ts
+++ src/app/game-code/game-code.component.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game-code',
@@ -13,16 +13,40 @@ import { RouterLink } from '@angular/router';
 export class GameCodeComponent {
   gameCode: string = '';
   characterCount: number = 0;
+  playerName: string = '';
+  nameError: string = ''; // Mensaje de error de validación
+
+  @Output() formSubmitted = new EventEmitter<{name: string}>();
+
+  validateName() {
+    // Validación para el nombre
+    if (!this.playerName) {
+      this.nameError = 'Por favor ingresa tu nombre.';
+    } else if (this.playerName.length < 3) {
+      this.nameError = 'El nombre debe tener al menos 3 caracteres.';
+    } else {
+      this.nameError = ''; // No hay errores
+    }
+  }
 
   showInstructions() {
     throw new Error('Metodo no impelmentado.');
   }
-
   updateCharacterCount(): void {
     this.characterCount = this.gameCode.length;
   }
-
   isValidCode(): boolean {
     return this.gameCode.length === 6;
+  }
+
+  saveName() {
+    this.validateName();
+    this.isValidCode();
+    if (!this.nameError && !this.isValidCode) {
+      // Emitir solo los datos necesarios
+      this.formSubmitted.emit({
+        name: this.playerName
+            });
+    }
   }
 }
